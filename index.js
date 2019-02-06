a7fb2d6f9c13eadc46621cdb82c8935d55455c64
Aoife O'Sullivan <aoifeosullivan19@gmail.com>
2019-02-06 11:39:24 +0000

    added index.html and index.js files

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3011);
+console.log("Server listening on port 3011");
